e74bc78caec0d6609263bd33fde3d5064536c8f6
rmiexception <rmiexception@gmail.com>
2019-11-25 14:44:40 +0900

    learn for jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { Component } from "react";
 
 function App() {
-  return <div><h1>Hello, world!</h1></div>;
+  return (
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => console.log("hoge hoge")}></input>
+    </React.Fragment>
+  );
 }
+
+/*
+class App extends Component {
+  render() {
+    return <div>Hello world!!!</div>;
+  }
+}
+*/
 
 export default App;
