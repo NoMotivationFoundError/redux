f94574f81896877386b21e0e3302cf327ff362f5
rmiexception <rmiexception@gmail.com>
2019-11-25 15:57:51 +0900

    learn prop-types

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const App = () => {
   const profiles = [
     {
-      name:"Shinzo",
-      age: 10,
+      name: "Shinzo",
+      age: 10
     },
     {
-      name:"Taro",
-      age: 100,
+      name: "Taro",
+      age: 100
     },
     {
-      name: 'hoge'
+      name: "Hoge",
+      age: 1223
     }
   ];
-  return <div>
-        {
-          profiles.map((profile, index) => {
-            return <User name={profile.name} age={profile.age} key={index} />
-          })
-        }
-        </div>
+  return (
+    <div>
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />;
+      })}
+    </div>
+  );
 };
 
-const User = (props) => {
-  return <div>
-    Hi, I am {props.name}, {props.age} years old !
-  </div>;
+const User = props => {
+  return (
+    <div>
+      Hi, I am {props.name}, {props.age} years old !
+    </div>
+  );
 };
 
-User.defaultProps = {
-  age : 1
-}
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
+};
 
 export default App;
