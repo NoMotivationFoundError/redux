934db22f9d30685b6b388a14b5814408caa7356b
rmiexception <rmiexception@gmail.com>
2019-11-25 15:27:56 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 
+/*
 function App() {
   return (
     <React.Fragment>
@@ -8,13 +9,18 @@ function App() {
     </React.Fragment>
   );
 }
-
-/*
-class App extends Component {
-  render() {
-    return <div>Hello world!!!</div>;
-  }
-}
 */
+
+const App = () => {
+  return <div>
+          <Cat />
+          <Cat />
+          <Cat />
+        </div>;
+};
+
+const Cat = () => {
+  return <div>Meow!!!</div>;
+};
 
 export default App;
